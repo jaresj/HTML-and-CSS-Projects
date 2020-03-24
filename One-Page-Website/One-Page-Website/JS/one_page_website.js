6cb1be1032bd5b2472f356965aca0f3054f5ffb5
jaresj <60714611+jaresj@users.noreply.github.com>
2020-03-23 19:26:45 -0700

    3/23 One-Page-Website

--- One-Page-Website/One-Page-Website/JS/one_page_website.js
+++ One-Page-Website/One-Page-Website/JS/one_page_website.js
@@ -28,6 +28,9 @@ function showSlides(n) {
     var captionText = document.getElementsByName("caption");
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
